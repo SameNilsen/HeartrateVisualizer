6becf5f43f79fa2bc2d8947ff3ee87e99812121d
Morten Nilsen <mort1.nilsen@gmail.com>
2023-08-09 14:54:20 +0200

    siste?

--- src/main/resources/static/frontPageJS.js
+++ src/main/resources/static/frontPageJS.js
@@ -81,7 +81,7 @@ function getTokens(authCode, scope) {
 };
 
 function nextFun() {
-    location.href = 'http://heartratevisualizer/stats.html';
+    location.href = 'http://www.heartratevisualizer/stats.html';
 };
 
 function showStart() {
