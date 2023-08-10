df60deccf7b664159561f0801d96f2847b27e8fa
Morten Nilsen <mort1.nilsen@gmail.com>
2023-08-10 12:54:16 +0200

    tester configuration bean

---
+++ src/main/java/heartratevisualizer/app/WebSecurityConfig.java
@@ -0,0 +1,18 @@
+package heartratevisualizer.app;
+
+// import org.springframework.configuration
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.*;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfiguration {
+
+  // @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.requiresChannel()
+      .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
+      .requiresSecure();
+  }
+}
