ab48f93fab8e05ed87a18826a09570475f39b87c
Morten Nilsen <mort1.nilsen@gmail.com>
2023-08-10 13:02:07 +0200

    tester om app.prop fix funker

--- src/main/java/heartratevisualizer/app/WebSecurityConfig.java
+++ src/main/java/heartratevisualizer/app/WebSecurityConfig.java
@@ -6,13 +6,13 @@ import org.springframework.context.annotation.*;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 
-@Configuration
-public class WebSecurityConfig extends WebSecurityConfiguration {
+// @Configuration
+// public class WebSecurityConfig extends WebSecurityConfiguration {
 
-  // @Override
-  protected void configure(HttpSecurity http) throws Exception {
-    http.requiresChannel()
-      .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
-      .requiresSecure();
-  }
-}
+//   @Override
+//   protected void configure(HttpSecurity http) throws Exception {
+//     http.requiresChannel()
+//       .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
+//       .requiresSecure();
+//   }
+// }
